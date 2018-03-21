4a04c43a5e5c4f48d0f83bf6d4cd5c77ae1fb6b9
sandumjacob <sandumjacobpersonal@gmail.com>
2018-03-21 18:09:13 -0500

    Added some OOP stuff

---
+++ Code/OOP/ColdBlooded.java
@@ -0,0 +1,7 @@
+package OOP;
+
+public interface ColdBlooded {
+
+    void raiseInternalTemperature();
+
+}
---
+++ Code/OOP/Lizard.java
@@ -0,0 +1,10 @@
+package OOP;
+
+public class Lizard extends Animal implements ColdBlooded {
+
+    @Override
+    public void raiseInternalTemperature() {
+        //Raise internal temperature
+    }
+
+}
