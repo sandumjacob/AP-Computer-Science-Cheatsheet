db3dde73428096d4180959ea410a4dfe89e4e119
sandumjacob <sandumjacobpersonal@gmail.com>
2018-03-22 19:57:57 -0500

    Added Factorial Example

--- Code/Recursion.java
+++ Code/Recursion.java
@@ -1,2 +1,21 @@
 public class Recursion {
+    /*
+    Trees are useful for determining the output of a recursive function
+    */
+    public static int factorial(int number) {
+
+        //Exit case
+        //This is needed so that the recursive method is not an infinite loop
+        if (number==1) {
+            return 1;
+        }
+
+        //Method calls itself
+        return number * factorial(number-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+    }
+
 }
